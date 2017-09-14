0d606660e04225daa919ef0e88436ad47ac47849
Marius Teller <marius.teller@modotex.com>
2017-09-14 21:34:52 +0200

    create from mission call

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Resistopia CIC Tool</h1>
 
-        <p class="lead">This is the test version fo the CIC tool</p>
+        <p class="lead">This is the test version of the CIC tool</p>
         <p class="lead">You will be able to click around freely without the need of a login to test out every feature.</p>
     </div>
 
