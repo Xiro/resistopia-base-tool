29060ba02d53715669b22c1672c86533868cc720
Marius Teller <marius.teller@modotex.com>
2018-07-26 18:08:33 +0200

    remove db config from working directory

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cic_new',
-//    'dsn' => 'mysql:host=localhost;dbname=cic_migration_test',
+//    'dsn' => 'mysql:host=localhost;dbname=cic_new',
+    'dsn' => 'mysql:host=localhost;dbname=cic_migration_test',
     'username' => 'cic',
     'password' => 'r1rmnn9mTOYtcwn4zaop',
     'charset' => 'utf8',
