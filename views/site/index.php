aba35df65170c1a68b07be7ab38d90a52e60166d
Marius Teller <marius.teller@modotex.com>
2018-07-26 12:04:46 +0200

    change index text

--- views/site/index.php
+++ views/site/index.php
@@ -4,14 +4,14 @@ use app\helpers\Html;
 
 /* @var $this yii\web\View */
 
-$this->title = 'Resistopia CIC Tool';
+$this->title = 'Resistopia Base Tool';
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center">
         <h1><?= $this->title ?></h1>
 
-        <p class="lead">This is the test version of the CIC tool</p>
+        <p class="lead">This is the test version of the base tool</p>
         <p class="lead">Feel free to go nuts</p>
     </div>
 
