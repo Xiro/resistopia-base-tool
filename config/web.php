78460c116f0486d4b1da894cf4f117e296c2d09c
Marius Teller <marius.teller@modotex.com>
2018-08-20 22:53:20 +0200

    define standard route for character registration subdomain

--- config/web.php
+++ config/web.php
@@ -74,6 +74,11 @@ $config = [
                 '<controller:\w+>/<id:\d+>'              => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'          => '<controller>/<action>',
+                [
+                    'pattern' => '/',
+                    'route' => 'character-registration/select',
+                    'host' => 'http://charakteranmeldung-resistopia.lost-ideas.com'
+                ]
             ),
         ],
         'assetManager' => [
