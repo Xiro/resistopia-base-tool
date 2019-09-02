0ac3eeed5b0ba994d053f13dfb216cf2420c15b6
Marius Teller <marius.teller@modotex.com>
2019-09-02 17:30:49 +0200

    new rotating icon

--- views/layouts/_icon-rotation.php
+++ views/layouts/_icon-rotation.php
@@ -7,14 +7,15 @@ echo Html::img(
         'class' => 'rotate',
         'speed' => 1500,
         'images' => json_encode([
-            Yii::getAlias('@web/img/resistance_logo_500.png'),
-            Yii::getAlias('@web/img/kat_stab_500.png'),
-            Yii::getAlias('@web/img/kat_medizin_500.png'),
-            Yii::getAlias('@web/img/kat_sicherheit_500.png'),
-            Yii::getAlias('@web/img/kat_technik_500.png'),
-            Yii::getAlias('@web/img/kat_kaempfer_500.png'),
-            Yii::getAlias('@web/img/kat_forschung_500.png'),
-            Yii::getAlias('@web/img/kat_coh_500.png'),
+//            Yii::getAlias('@web/img/resistance_logo_500.png'),
+//            Yii::getAlias('@web/img/kat_stab_500.png'),
+//            Yii::getAlias('@web/img/kat_medizin_500.png'),
+//            Yii::getAlias('@web/img/kat_sicherheit_500.png'),
+//            Yii::getAlias('@web/img/kat_technik_500.png'),
+//            Yii::getAlias('@web/img/kat_kaempfer_500.png'),
+//            Yii::getAlias('@web/img/kat_forschung_500.png'),
+//            Yii::getAlias('@web/img/kat_coh_500.png'),
+            Yii::getAlias('@web/img/phoenix_500.png'),
         ]),
     ]
 );
