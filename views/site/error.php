6a2ac706fa9d9876b0ab00289710f445ee884ddf
Marius Teller <marius.teller@modotex.com>
2019-09-02 11:38:42 +0200

    new error page

--- views/site/error.php
+++ views/site/error.php
@@ -11,18 +11,26 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h3><?= Html::encode($this->title) ?></h3>
 
-    <p style="font-size: 24px; margin-bottom: 15px">
+    <p style="font-size: 20px; margin-bottom: 15px">
         <?= nl2br(Html::encode($message)) ?>
     </p>
 
-
+    <h1>
+        Ein Fehler ist aufgetreten, aber das liegt nicht an Ihnen!
+    </h1>
     <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
+        Bitte sammeln Sie alle Informationen, die zur Beseitigung des Fehlers hilfreich sind. Hierzu gehören:
+        <ul>
+            <li>Was haben Sie angeklickt, bevor der Fehler aufgetreten ist?</li>
+            <li>Was haben Sie unmittelbar davor gemacht?</li>
+            <li>Haben Sie ein Formular ausgefüllt? Wenn ja, mit welchen Daten?</li>
+            <li>Tritt dieser Fehler immer auf, wenn Sie die Aktion nochmal ausführen?</li>
+        </ul>
+        Melden Sie sich danach mit einer detaillierten Beschreibung des Fehlers im CIC bei Nico Schulte.<br>
+        <br>
+        Vielen Dank.
     </p>
 
 </div>
